e78678dea1b1c963bdb71ca29e58c02edea9963b
mohamed mohamed <>
2021-06-01 04:49:12 +0000

    update to hello world!

--- hello.js
+++ hello.js
@@ -1,7 +1,9 @@
 function hello() {
 	// TODO: change this string so that your program prints "Hello world!" 
 	// when you hit the replit run button
-	console.log("Hello World!");
+	
+  return "Hello World!";
+
   
 }
  
